b7cdea00f065805cf578a3673d1ceb90bbc7be9c
kentana20 <civic.b16b@gmail.com>
2017-05-28 19:20:01 +0900

    add scraping data from sky

--- index.js
+++ index.js
@@ -1,17 +1,33 @@
 /* eslint-disable no-console */
 'use strict';
 
-const request = require('request');
-const cheerio = require('cheerio');
+const cli = require('cheerio-httpcli');
 
-let uri = 'http://www.google.co.jp/';
+const url = 'http://www.skysports.com/premier-league-news';
 
-request({uri: uri}, (error, res, body) => {
-    if (!error && res.statusCode == 200) {
-        let $ = cheerio.load(body);
-        console.log(res.request.href);
-        console.log($('title').text());
-    } else {
-        console.log('Error:' + error.code);
+cli.fetch(url, {initialCollectionKey: 1}, (error, $, res) => {
+    if (error || res.statusCode != 200) {
+        console.log('Error:' + error.message);
+        return;
     }
+    let target = [];
+    let id, title, head, url;
+
+    // Get News Data
+    // console.log('block: ' + $('a[class=news-list__figure]').parent().attr('data-id'));
+    $('a[class=news-list__headline-link]').each((idx) => {
+        console.log(`idx: ${idx}`);
+        id = $('a[class=news-list__figure]').eq(idx).parent().attr('data-id');
+        title = $('h4[class=news-list__headline]').eq(idx).text();
+        head = $('p[class=news-list__snippet]').eq(idx).text();
+        url = $('h4[class=news-list__headline]').eq(idx).find('a').attr('href');
+
+        target[idx] = [
+            id.trim(),
+            title.trim(),
+            head.trim(),
+            url.trim()
+        ];
+        console.log(`idx: ${idx} --- ${JSON.stringify(target[idx])}`);
+    });
 });
